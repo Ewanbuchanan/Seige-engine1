e25fc965ff7b6769c86f86f8b1090399f38fc916
logie87 <117318139+logie87@users.noreply.github.com>
2022-11-02 21:48:07 -0700

    servo.css
    controls the servo if using arduino, 
    servo pin = 1

---
+++ servo.css
@@ -0,0 +1,13 @@
+#include<Servo.h>
+Servo servo;
+//establish arduino to console communication & assign pin to arduino
+void setup() {
+  servo.attach(3);
+  Serial.begin(9600);
+}
+//loop servo motion 0 -> 90 degrees
+void loop () {
+ servo.write(90);
+ delay(500);
+ servo.write(0);
+}
