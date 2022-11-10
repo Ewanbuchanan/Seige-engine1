04c03332416f4261c133b389e475e219a2d3089c
Ewanbuchanan <ewanbuchanan8@gmail.com>
2022-11-10 15:31:54 -0800

    made code lmao

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import RPi.GPIO as Io
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+sense_pin = 11
+servo_pin = 13
+dspl_pin = 15
+servo1 = Io.PWM(13, 50)
+Io.setmode(Io.BOARD)
+Io.setup(sense_pin, Io.IN)
+Io.setup(servo_pin, Io.OUT)
+Io.setup(dspl_pin, Io.OUT)
+last_time = time.time()
+this_time = time.time()
+rpm = 0
+
+servo1.start(0)
+duty = 0
+def rpmsense(channel):
+    global rpm, this_time, last_time
+    this_time = time.time()
+    rpm = (1 / (this_time - last_time)) * 60
+    print('RPM = {:7.1f}'.format(rpm))
+    last_time = this_time
+    return ()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+Io.add_event_detect(sense_pin, Io.RISING, callback= rpmsense, bouncetime=1)
+
+if rpm < 1000:
+    servo1.ChangeDutyCycle(12)
+else:
+    pass
+
+try:
+    for x in range(0, ):
+        time.sleep(0.5)
+except:
+    time.sleep(2)
+    Io.remove_event_detect(sense_pin)
+    Io.cleanup()
+    servo1.stop()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
